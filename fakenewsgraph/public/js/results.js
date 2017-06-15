75c2cae601fc85025a57846cc241b5aea1b0521d
Lasse Todt <lassetodt@mailbox.org>
2017-06-15 12:55:32 +0200

    api dev: created example for keyword-results

--- fakenewsgraph/public/js/results.js
+++ fakenewsgraph/public/js/results.js
@@ -1,5 +1,20 @@
-$(document).ready(function(){
-    $.get('static-api-req/results.json', function(res){
-        console.log(res.name);
-    })
+$(document).ready(function () {
+    $.get('static-api-req/results.json', function (res) {
+        if (res.results) {
+            res.results.forEach(function (result) {
+                $('#tablebody').append(
+                    '<tr class="clickable-row" data-href="/analysis?id=' + result.id + '">' +
+                        '<td>' + result.title + '</td>' +
+                        '<td>' + result.type + '</td>' +
+                        '<td>' + result.fakeState + '</td>' +
+                    '</tr>'
+                )
+            });
+            $(".clickable-row").click(function() {
+                window.location = $(this).data("href");
+            });
+        }
+    });
+
+
 });
