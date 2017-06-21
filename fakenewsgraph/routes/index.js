85b2d98b39619427f476adf8be682ed0f4f58797
dani <daniel.dimitrov@campus.tu-berlin.de>
2017-06-21 04:01:53 +0200

    21.07 Progress - added the header and footer. Header still needs work

--- fakenewsgraph/routes/index.js
+++ fakenewsgraph/routes/index.js
@@ -5,10 +5,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/", function(req, res){
-    res.render('index');
-});
-
-router.get("/wordcloud", function(req, res){
     res.render('wordcloud');
 });
 
