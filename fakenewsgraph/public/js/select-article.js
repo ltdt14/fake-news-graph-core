fe9c649035db1921c433101d2cdc6d9a5f1093d3
dani <daniel.dimitrov@campus.tu-berlin.de>
2017-07-04 19:17:57 +0200

    Edited the example json file to have real link and titles

--- fakenewsgraph/public/js/select-article.js
+++ fakenewsgraph/public/js/select-article.js
@@ -1,23 +1,62 @@
+function extractHostname(url) {
+    var hostname;
+    //find & remove protocol (http, ftp, etc.) and get hostname
+
+    if (url.indexOf("://") > -1) {
+        hostname = url.split('/')[2];
+    }
+    else {
+        hostname = url.split('/')[0];
+    }
+
+    //find & remove port number
+    hostname = hostname.split(':')[0];
+    //find & remove "?"
+    hostname = hostname.split('?')[0];
+
+    return hostname;
+}
+
+function extractRootDomain(url) {
+    var domain = extractHostname(url),
+        splitArr = domain.split('.'),
+        arrLen = splitArr.length;
+
+    //extracting the root domain here
+    if (arrLen > 2) {
+        domain = splitArr[arrLen - 2] + '.' + splitArr[arrLen - 1];
+    }
+    return domain;
+}
+
 $(document).ready(function () {
     console.log("here 2");
     $.get('static-api-req/articles.json', function (res) {
         console.log("here 2");
         if (res.results) {
             res.results.forEach(function (result) {
-                console.log(result.title);
                 $('#grid-container').append(
 
                     '<div class="side-pad col-lg-4 col-md-6 col-sm-6 col-xs-12" ' +
                     'data-href="/analysis?id=' + result.id + '">'
                         + '<div class="clickable-article col-lg-12">'
                             + '<div class="thumbnail-container">'
-                                + '<img class="article-thumbnail" src="'+ result.thumbnail + '">'
+                                + '<a href="/analysis?id=' + result.id + '"> '
+                                    + '<img class="article-thumbnail" src="'+ result.thumbnail + '"> '
+                                + '</a>'
                             + '</div>'
+
                             + '<div class="article-info">'
-                                + '<h3>' + result.title + '</h3>'
+                                + '<a href="/analysis?id=' + result.id + '"> '
+                                    + '<h3>' + result.title + '</h3>'
+                                + '</a>'
                                 + '<table>'
                                     + '<tr>'
-                                        + '<td> Source: ' + result.source + '</td>'
+                                        + '<td> Source: '
+                                            + '<a href="' + result.source + '"> '
+                                                + extractRootDomain(result.source)
+                                            + '</a>'
+                                        + '</td>'
                                         + '<td class="text-right"> Retweets: ' + result.retweets + '</td>'
                                     + '</tr>'
 
@@ -29,10 +68,6 @@ $(document).ready(function () {
                         + '</div>'
                     + '</div>'
                 )
-            }
-            );
-            $(".clickable-article").click(function() {
-                window.location = $(this).data("href");
             });
         }
     });
